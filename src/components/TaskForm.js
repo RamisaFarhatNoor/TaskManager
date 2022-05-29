32caa99b63cb739565d445dc52f198c7b8db0ef2
Ramisa Noor <noor.ramisafarhat@gmail.com>
2022-05-29 21:56:24 +0600

    first commit

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React,{useContext,useState} from 'react';
+import TaskListContext  from '../context/TaskListContext';
 
 const TaskForm = () => {
+   const{addTask}= useContext(TaskListContext);
+   const[title,setTitle]= useState("");
+   const handleChange = e =>{
+       setTitle(e.target.value);
+       console.log(title);
+   };
+   const handleSubmit = e =>{
+       e.preventDefault();
+       addTask(title)
+   };
   return (
     <div>
-      <form action='' className='form'>
-          <input type="text "
+      <form onSubmit={handleSubmit} className='form'>
+          <input
+          onChange={handleChange}
+          value={addTask}
+          type="text "
           className="text-input"
           placeholder="Add a task"/>
           <div className='buttons'>
--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -1,14 +1,23 @@
 import React, {createContext,useState} from "react";
+import {v1 as uuid} from "uuid";
 export const TaskListContext= createContext()
 
 const TaskListContextProvider = props =>{
-    const[task,setTask] = useState([{ title: "Read the book", id: 1},
+    const[task,setTask] = useState([
+    { title: "Read the book", id: 1},
     { title: "Wash the Car", id: 2},
-    { title: "Organize the closet", id: 3}]);
-    return(<TaskListContext.Provider value={{task}}>
+    { title: "Organize the closet", id: 3}
+    ]);
+    const addTask =(title)=>{
+        setTask([...task, {title, id: uuid()}]);
+
+    }
+    return(
+    <TaskListContext.Provider value={{task, addTask}}>
         {props.children}
 
-    </TaskListContext.Provider>)
+    </TaskListContext.Provider>
+    );
 };
 
 export default TaskListContextProvider;
