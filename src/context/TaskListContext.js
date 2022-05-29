108bb33cd4c5da5fdc144e2a40e18620d064777f
Ramisa Noor <noor.ramisafarhat@gmail.com>
2022-05-29 20:16:45 +0600

    first commit

--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -2,10 +2,10 @@ import React, {createContext,useState} from "react";
 export const TaskListContext= createContext()
 
 const TaskListContextProvider = props =>{
-    const[tasks,setTask] = useState([{ task: "Read the book", id: 1},
-    { task: "Wash the Car", id: 2},
-    { task: "Organize the closet", id: 3}]);
-    return( <TaskListContext.Provider value={{tasks}}>
+    const[task,setTask] = useState([{ title: "Read the book", id: 1},
+    { title: "Wash the Car", id: 2},
+    { title: "Organize the closet", id: 3}]);
+    return( <TaskListContext.Provider value={{task}}>
         {props.children}
 
     </TaskListContext.Provider>)
